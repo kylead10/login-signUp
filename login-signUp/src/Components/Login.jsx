9bd439776602881f63925581d1965b58678a02f4
kylead10 <adiokayode111@gmail.com>
2023-10-20 17:24:18 +0100

    adding functionality

--- login-signUp/src/Components/Login.jsx
+++ login-signUp/src/Components/Login.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Components/Login.css';
 import { MdEmail } from 'react-icons/md';
 import { RiLockPasswordFill } from 'react-icons/ri';
 import { BsFillFilePersonFill } from 'react-icons/bs';
 
 const Login = () => {
+  const [action, setAction] = useState('Sign Up');
+
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>Sign Up</div>
+        <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
       <div className='inputs'>
@@ -29,8 +31,13 @@ const Login = () => {
         Forgot Password? <span>Click Here!</span>
       </div>
       <div className='submit-container'>
-        <div className='submit'>Sign Up</div>
-        <div className='submit'>Login</div>
+        <div className={action === 'Login' ? 'submit gray' : 'submit'}>
+          Sign Up
+        </div>
+        <div style={{ margin: '13px' }}></div>
+        <div className={action === 'Sign Up' ? 'submit gray' : 'submit'}>
+          Login
+        </div>
       </div>
     </div>
   );
